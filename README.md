c16de3bd65cc1ad1e1c0090e4efd7e5b483c6962
Shreyas Kapur <shreyask@mit.edu>
2017-03-20 09:33:49 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+## Minimalistic Portfolio Template
+
+### How do I use this?
+Locally
+From the root directory, use:
+```
+python -m SimpleHTTPServer
+```
+
+Use any static webserver to deploy.
---
+++ js/main.js
@@ -0,0 +1,50 @@
+String.prototype.repeat = function(num) {
+    return new Array(num + 1).join(this);
+}
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function getRandomDirection() {
+	return {x: Math.random()*2-1, y: Math.random()*2-1};
+}
+
+$(document).ready(function() {
+	/* Code to generate dummy data. */
+	// var html = $('.columns').html();
+	// var replicated = html.repeat(4);
+	// $('.columns').html(replicated);
+
+	$('img').each(function(i) {
+		$(this).attr('src', '/img/' + (i+1) + '.jpg');
+	});
+
+	var area = [0,0,$('body').width(),$('body').height()]
+	$('body').imagesLoaded(function() {
+		$('a').each(function(i) {
+			$(this).throwable({
+				autostart: true,
+				impulse: {
+					f: 50,
+					p: getRandomDirection()
+				},
+				gravity: {
+					x: 0, y: 0
+				},
+				bounce: 0.5,
+				containment: area,
+				damping: 5,
+				shape: "box"
+			});
+		});
+	});
+});
+
+
